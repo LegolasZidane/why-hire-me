48e62ba39db26d621772d7eceb9ce67e9e13f758
LegolasZidane <laraibaa6@gmail.com>
2025-07-14 05:48:35 +0530

    Disguised Procrastination

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,13 +1,13 @@
 function LandingPage() {
   return (
-    <div className="d-flex h-100 text-center text-bg-dark align-items-center justify-content-center">
+    <div className="landing-page-body d-flex text-center text-bg-dark align-items-center justify-content-center">
       <div>
-        <h1>Why Hire Me?</h1>
+        <h1>Why Hire Me?(Fresher Edition)</h1>
         <p className="lead">
-          This is a solution to a real life problem of a CS Graduate finding a job in 2025.
+          This is a solution to a real life problem of a 2021 CS Graduate finding a job in 2025.
         </p>
         <a href="#" className="btn btn-lg btn-light fw-bold border-white bg-white">
-          Work in Progres.
+          Work in Progress
         </a>
       </div>
     </div>
