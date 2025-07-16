a3f786c27df049ea405fad8b7e693fce50ca0823
LegolasZidane <laraibaa6@gmail.com>
2025-07-17 01:10:08 +0530

    inserting components only

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import LandingPage from './components/LandingPage.jsx';
+import NavBar from './components/NavBar.jsx';
+import ProjectsPage from './components/ProjectsPage.jsx';
+import ConsistencyPage from './components/ConsistencyPage.jsx';
+import Resume from './components/Resume.jsx';
+import AboutMe from './components/AboutMe.jsx';
 
 function App() {
 
   return (
-    
+    <div className="spa">
+    <NavBar />
     <LandingPage />
-    
+    <ProjectsPage />
+    <ConsistencyPage />
+    <Resume />
+    <AboutMe />
+    </div>
   );
 }
 
