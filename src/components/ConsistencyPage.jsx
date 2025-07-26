eb8ce4a428788daf7497b47da1de3965d8f47320
LegolasZidane <laraibaa6@gmail.com>
2025-07-26 17:26:35 +0530

    LC and GH contributions match with their profiles

--- src/components/ConsistencyPage.jsx
+++ src/components/ConsistencyPage.jsx
@@ -12,7 +12,7 @@ function setValue(contribution){
         return 10;
 }
 
-function generateContributions(startDate){
+function generateContributions(startDate, data){
 
     const daysInMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     // const nameOfMonth = ["", "Jan", "Feb", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -30,7 +30,7 @@ function generateContributions(startDate){
     let days = 0;
     let week = [];
 
-    let input = [13, 3, 6, 13, 2, 0, 33, 12, 2, 10, 1, 14, 0, 0, 11, 25, 5, 12, 9, 11, 0, 2, 3, 5, 9, 10, 23, 6, 10, 11, 13, 13, 3, 2, 19, 8, 12, 4, 0, 0, 0, 12, 15, 20, 0, 12, 3, 4, 4, 3, 16, 19, 2, 19, 16, 5, 1, 6, 16, 6, 3, 1, 4, 1, 7, 12, 17, 0, 0, 15, 10, 15, 7, 1, 20, 8, 4, 7, 4, 5, 1, 1, 8, 13, 3, 1, 6, 1, 5, 2, 3, 1, 1, 1, 1, 1, 1, 1, 0, 1, 3, 1, 0, 1, 1, 1, 3, 0, 2, 5, 3, 4, 2, 1, 2, 0, 1, 1, 4, 2, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 0, 1, 1];
+    let input = data;
     let day = 0;
 
     for( let i = startMonth; i <= endMonth; i++ ){
@@ -64,7 +64,7 @@ function makeGraphGH() {
     let graph = document.querySelector('.graphGH');
     graph.innerHTML = "";
     //Please give date in DD/MM/YYYY format, thank you.
-    let contributions = generateContributions("19/05/2025");
+    let contributions = generateContributions("19/05/2025", [3, 1, 3, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 19, 13, 0, 6, 1, 1, 4, 5, 4, 1, 17, 1, 1, 1, 0, 2, 0]);
 
     for( let i = 0; i < contributions.length; i++ ){
 
@@ -85,10 +85,10 @@ function makeGraphGH() {
 
 function makeGraphLC() {
 
-    let graph = document.querySelector('.graphLC');
+    let graph = document.querySelector('.graphLC'); 
     graph.innerHTML = "";
     //Please give date in DD/MM/YYYY format, thank you.
-    let contributions = generateContributions("09/03/2025");
+    let contributions = generateContributions("09/03/2025", [13, 3, 6, 13, 2, 0, 33, 12, 2, 10, 1, 14, 0, 0, 11, 25, 5, 12, 9, 11, 0, 2, 3, 5, 9, 10, 23, 6, 10, 11, 13, 13, 3, 2, 19, 8, 12, 4, 0, 0, 0, 12, 15, 20, 0, 12, 3, 4, 4, 3, 16, 19, 2, 19, 16, 5, 1, 6, 16, 6, 3, 1, 4, 1, 7, 12, 17, 0, 0, 15, 10, 15, 7, 1, 20, 8, 4, 7, 4, 5, 1, 1, 8, 13, 3, 1, 6, 1, 5, 2, 3, 1, 1, 1, 1, 1, 1, 1, 0, 1, 3, 1, 0, 1, 1, 1, 3, 0, 2, 5, 3, 4, 2, 1, 2, 0, 1, 1, 4, 2, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 2, 0]);
 
     for( let i = 0; i < contributions.length; i++ ){
 
