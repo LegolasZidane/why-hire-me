d74d8cebe8322d26a5cf486c7baa3b4d3319a800
LegolasZidane <laraibaa6@gmail.com>
2025-07-29 08:44:11 +0530

    new page additions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import LandingPage from './components/LandingPage.jsx';
 import NavBar from './components/NavBar.jsx';
+import LandingPage from './components/LandingPage.jsx';
 import ExplanationPage from './components/ExplanationPage.jsx';
 import ProjectsPage from './components/ProjectsPage.jsx';
 import ConsistencyPage from './components/ConsistencyPage.jsx';
+import TheGrind from './components/TheGrind.jsx';
+import ConceptsPage from './components/ConceptsPage.jsx';
 import Resume from './components/Resume.jsx';
 import AboutMe from './components/AboutMe.jsx';
 
@@ -15,6 +17,8 @@ function App() {
       <ExplanationPage />
       <ProjectsPage />
       <ConsistencyPage />
+      <TheGrind />
+      <ConceptsPage />
       <Resume />
       <AboutMe />
     </div>
